d96c9e8aa5f01ce27a02723ba5c10c5f6bbf577c
Camilobarre <barrecami@gmail.com>
2024-07-22 07:12:12 -0500

    Se crea el repositorio

---
+++ Models/EjemploEstatico.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_static.Models;
+public class EjemploEstatico    
+{
+    public static string Nombre {get; set;}
+    public static string Apellido {get; set;}
+
+    public EjemploEstatico()
+    {
+        Nombre = "Camilo";
+        Apellido = "Barreneche Escobar";
+    }
+
+    public static void Saludar(){
+        Console.WriteLine($"Hola a todos, soy {Nombre} {Apellido}");
+    }         
+}
--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@ var ejemplo1 = new Ejemplo();
 Console.WriteLine("Nombre=> "+ ejemplo1.Nombre);
 Console.WriteLine("Apellido => "+ ejemplo1.Apellido);
 ejemplo1.Saludar();
+
+//Uso de propiedades y métodos pero con el STATIC
+//Sirve para acceder a las propiedades sin necesidad de instanciarlas
+EjemploEstatico.Nombre ="Camilo";
+EjemploEstatico.Apellido = "Barreneche Escobar";
+EjemploEstatico.Saludar();
+Console.WriteLine("Nombre=> "+ ejemplo1.Nombre);
+Console.WriteLine("Apellido => "+ ejemplo1.Apellido);
+ejemplo1.Saludar();
